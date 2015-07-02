1d9544f788d409a7a1894049f169a67758306344
suvek <ersuvek@gmail.com>
2015-07-02 19:01:21 +0530

    add template

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'hellodjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "hellodjango/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
